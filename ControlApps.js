d91e22cf4f9ba01ecd18fc56e9331fb8d22850ad
hamzy <ilham5273637@gmail.com>
2025-12-04 14:28:49 +0700

    Add files via upload

---
+++ ControlApps.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8581322661:AAEtsJaR7UjO0A5KYXJQ5PcLXCrBHBNPfnM", 
+  owner: "8312382874", 
+  port: "2291", // Ini Wajib Jangan Diubah
+  ipvps: "http://rizz.myserverku.cloud" // Jangan Diubah Nanti Eror!!
+};
